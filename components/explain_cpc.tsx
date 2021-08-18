84b7730999eb0ed757e857b3eb5238f9bb3613ef
einemutigeschlacht <eine.schlacht@gmail.com>
2021-08-19 00:10:54 +0700

    :hammer: fix(core): change the color of components.

--- components/explain_cpc.tsx
+++ components/explain_cpc.tsx
@@ -72,14 +72,14 @@ export default function Explain({}: IExplainProps) {
         <div className="py-12 bg-white">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="lg:text-center">
-                    <h2 className="text-base text-indigo-600 font-medium tracking-wide uppercase">
-                        Transactions
+                    <h2 className="text-base text-[#8318bd] font-medium tracking-wide uppercase">
+                        Types of Axis
                     </h2>
                     <p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        A better way to send money
+                        Explain what each axis means
                     </p>
                     <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                        Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.
+                        Each axis is an indicator of your ideology after you answer <br/> our ideological analysis questions.
                     </p>
                 </div>
 
@@ -88,7 +88,7 @@ export default function Explain({}: IExplainProps) {
                         {explains.map((explain) => (
                             <div key={explain.name} className="relative">
                                 <dt>
-                                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-[#8318bd] text-white">
                                         <explain.icon className="h-6 w-6" aria-hidden="true" />
                                     </div>
                                     <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
